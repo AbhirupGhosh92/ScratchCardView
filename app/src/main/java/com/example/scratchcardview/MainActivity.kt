3a94d6ae78ce2b2085537bd350bf640dd5405c9c
Abhirup Ghosh <joshmcgrath123@gmail.com>
2020-08-07 03:01:28 +0530

    added example with an actual card

--- app/src/main/java/com/example/scratchcardview/MainActivity.kt
+++ app/src/main/java/com/example/scratchcardview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.scratchcardview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ezzzio.scratchcard.ScratchCardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
